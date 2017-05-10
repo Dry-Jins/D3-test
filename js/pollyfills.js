2cc902bcae5f094dbb027eb08addd2355a063edd
skyjin <dryjins@gmail.com>
2017-05-10 09:48:03 +0300

    circos test

---
+++ js/pollyfills.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dryji on 2017-05-04.
+ */
+// poly-fill//
+/*
+var $ =  function (selector) {
+    var sel = document.querySelectorAll(selector);
+    if (sel.length == 1) return sel[0];
+    else return sel;
+};
+*/
+var log = console.log.bind(console);
+var dir = console.dir.bind(console);
+var replace = function(string) { return string.replace(/[^a-z0-9]/gi,""); };
